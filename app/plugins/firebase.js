efa84d71ea7c0137b9d320480dcdb12d43689b9a
MyPoZi <kxx555u.u@gmail.com>
2020-01-14 04:18:37 +0900

    first

--- app/plugins/firebase.js
+++ app/plugins/firebase.js
@@ -3,12 +3,12 @@ import * as firebase from 'firebase/app'
 // TODO : fix
 if (!firebase.apps.length) {
   firebase.initializeApp({
-    apiKey: '<API_KEY>',
-    authDomain: '<AUTH_DOMAIN>',
-    databaseURL: '<DATABASE_URL>',
-    projectId: '<PROJECT_ID>',
-    storageBucket: '<STORAGE_BUCKET>',
-    messagingSenderId: '<MESSAGING_SENDER_ID>'
+    apiKey: 'AIzaSyDI4s3XY2is7vvNR5UFk6WpVgmuCu7fwcE',
+    authDomain: 'sample-blog-app-a858c.firebaseapp.com',
+    databaseURL: 'https://sample-blog-app-a858c.firebaseio.com',
+    projectId: 'sample-blog-app-a858c',
+    storageBucket: 'sample-blog-app-a858c.appspot.com',
+    messagingSenderId: '23551322611'
   })
 }
 
